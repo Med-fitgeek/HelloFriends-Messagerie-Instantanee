3ccb330a41c208b1b4988b433271b7a1ae90a380
Medoune <ndongomedoune.ndiaye@gmail.com>
2025-06-23 14:35:00 +0200

    feat: updated imports in frontend

--- chat-client/src/app/chat/chat/chat.component.ts
+++ chat-client/src/app/chat/chat/chat.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { ChatService } from '../../services/chat.service';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
+  messages: { user: string, message: string }[] = [];
+  currentMessage = '';
+  currentUser = 'User'; // à récupérer depuis le token
 
+  constructor(private chatService: ChatService) {}
+
+  ngOnInit(): void {
+    this.chatService.startConnection();
+    this.chatService.addReceiveMessageListener((user, message) => {
+      this.messages.push({ user, message });
+    });
+  }
+
+  sendMessage(): void {
+    if (this.currentMessage.trim()) {
+      this.chatService.sendMessage(this.currentUser, this.currentMessage);
+      this.currentMessage = '';
+    }
+  }
 }
